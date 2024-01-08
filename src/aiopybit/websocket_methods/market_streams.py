470512a2515995391ee3360dbfb48b231694536e
Tsunami43 <n3m0_22@mail.ru>
2024-01-08 14:20:00 +0300

    Add orderbook and trades subscriptions

--- src/aiopybit/websocket_methods/market_streams.py
+++ src/aiopybit/websocket_methods/market_streams.py
@@ -24,3 +24,36 @@ class ByBitPublicStreamsMixin:
 		logger.info('Subscribed to %s', topic)
 
 		return topic
+
+	async def subscribe_to_orderbook(
+		self,
+		category: str,
+		symbol: str,
+		on_message: Callable,
+		depth: int = 50,
+	) -> str:
+		"""Subscribe to orderbook updates."""
+		websocket = await self.get_websocket('public.' + category)
+		topic = f'orderbook.{depth}.{symbol}'
+		websocket.topic_handlers[topic] = on_message
+
+		await websocket.send(op='subscribe', args=[topic])
+		logger.info('Subscribed to %s', topic)
+
+		return topic
+
+	async def subscribe_to_public_trades(
+		self,
+		category: str,
+		symbol: str,
+		on_message: Callable,
+	) -> str:
+		"""Subscribe to public trades."""
+		websocket = await self.get_websocket('public.' + category)
+		topic = f'publicTrade.{symbol}'
+		websocket.topic_handlers[topic] = on_message
+
+		await websocket.send(op='subscribe', args=[topic])
+		logger.info('Subscribed to %s', topic)
+
+		return topic
