b46a4965a90d138e785091987999a46791fb479c
Tsunami43 <n3m0_22@mail.ru>
2024-02-12 15:45:00 +0300

    Add market data HTTP methods

--- src/aiopybit/http_methods/__init__.py
+++ src/aiopybit/http_methods/__init__.py
@@ -1,11 +1,1 @@
-from .account_methods import AccountMixin
-from .market_methods import MarketMixin
-from .order_methods import OrderMixin
-from .position_methods import PositionMixin
-
-__all__ = [
-	'AccountMixin',
-	'OrderMixin',
-	'MarketMixin',
-	'PositionMixin',
-]
+"""HTTP method mixins for ByBit API."""
