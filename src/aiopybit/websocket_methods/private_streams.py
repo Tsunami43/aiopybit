bb52b6e4d3bf7d591b16fbf2890a6a1b4dbed92a
Tsunami43 <n3m0_22@mail.ru>
2024-04-02 11:45:00 +0300

    Add execution and wallet subscriptions

--- src/aiopybit/websocket_methods/private_streams.py
+++ src/aiopybit/websocket_methods/private_streams.py
@@ -30,3 +30,25 @@ class ByBitPrivateStreamsMixin:
 		logger.info('Subscribed to %s', topic)
 
 		return topic
+
+	async def subscribe_to_execution(self, on_message: Callable) -> str:
+		"""Subscribe to execution updates."""
+		websocket = await self.get_websocket('private')
+		topic = 'execution'
+		websocket.topic_handlers[topic] = on_message
+
+		await websocket.send(op='subscribe', args=[topic])
+		logger.info('Subscribed to %s', topic)
+
+		return topic
+
+	async def subscribe_to_wallet(self, on_message: Callable) -> str:
+		"""Subscribe to wallet balance updates."""
+		websocket = await self.get_websocket('private')
+		topic = 'wallet'
+		websocket.topic_handlers[topic] = on_message
+
+		await websocket.send(op='subscribe', args=[topic])
+		logger.info('Subscribed to %s', topic)
+
+		return topic
